65c478b4a1605b7f16886d7085a629644328bee6
Alexandru-Mihai Manolescu <alex.inntekt@gmail.com>
2019-07-12 19:32:24 +0300

    Initial Commit

---
+++ play/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  play
+//
+//  Created by Alexandru-Mihai Manolescu on 12/07/2019.
+//  Copyright © 2019 Alexandru-Mihai Manolescu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
