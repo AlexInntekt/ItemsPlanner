1300d3b0c803a9babd0ecbee043508985923202d
Alexandru-Mihai Manolescu <alex.inntekt@gmail.com>
2019-07-14 09:48:53 +0300

    first commit

--- play/ViewController.swift
+++ play/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+var list=["banana","portocala","ciucamba"]
+
+
+
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = list[indexPath.row]
+        return cell;
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tbv1.dataSource = self
+        tbv1.delegate = self
+        
+
+        let ref = Database.database().reference(withPath: "items")
+        let elem = ref.child("balon_rosu").child("intervals").child("2")
+        
+        elem.setValue("12-26 August")
+        
+
+        
+        
+        //ref.child("items").child("balon_rosu").setValue("maaaa")
+    }
+    
+    @IBOutlet weak var bt1: UIButton!
+    @IBAction func bt1(_ sender: Any)
+    {
+        
     }
 
-
+    @IBOutlet weak var tbv1: UITableView!
+    
 }
 
