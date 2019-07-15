3ee8789527cada81790618d1b830a08f5acb16a5
Alexandru-Mihai Manolescu <alex.inntekt@gmail.com>
2019-07-15 09:05:37 +0300

    auth UI

---
+++ play/CreareCont.swift
@@ -0,0 +1,73 @@
+//
+//  CreareCont.swift
+//
+//
+//  Created by Alexandru-Mihai Manolescu on 14/07/2019.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+
+class CreareCont: UIViewController
+{
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var signup: UIButton!
+    @IBAction func signup(_ sender: Any)
+    {
+        self.signup.setTitle("Se procesează..", for: .normal)
+        
+        if ((email.text != nil) &&  (password.text != nil))
+        {
+            
+            Auth.auth().createUser(withEmail: self.email.text!, password: self.password.text!, completion: { (user, error) in
+                print("\n\n # Trying to create a new user...\n")
+                
+                if error != nil
+                {
+                    print("Error detected. Finding in console: g7i23h49fofou23go ",error)
+                }
+                
+                self.send_verification_email()
+                
+            })
+        }
+        
+        
+    }
+    
+    
+    func send_verification_email()
+    {
+        Auth.auth().currentUser?.sendEmailVerification { (error) in
+            if error != nil
+            {
+                print("Error in sending verification email. Finding code: w8hgf0qhg02h3g ", error)
+            }
+            
+        }
+    }
+    
+    func setupUI()
+    {
+        self.signup.layer.borderWidth=1
+        self.signup.layer.borderColor=UIColor.gray.cgColor
+        self.signup.layer.cornerRadius=17
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+        print("CreareCont este in deschis")
+        
+       
+        setupUI()
+    }
+}
---
+++ play/PanouPrimire.swift
@@ -0,0 +1,25 @@
+//
+//  PanouPrimire.swift
+//  
+//
+//  Created by Alexandru-Mihai Manolescu on 14/07/2019.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+
+class PanouPrimire: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+        print("PanouPrimire este in deschis")
+        
+        
+    }
+}
