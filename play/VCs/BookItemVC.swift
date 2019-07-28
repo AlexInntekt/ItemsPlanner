8b07f4cf4eba49c6ba0ed187d4c45d40ff54861b
alexinntekt <alex.inntekt@gmail.com>
2019-07-28 13:03:11 +0300

    start integrating the calendar in a specific VC

---
+++ play/VCs/BookItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  BookItemVC.swift
+//  play
+//
+//  Created by Alexandru-Mihai Manolescu on 28/07/2019.
+//  Copyright © 2019 Alexandru-Mihai Manolescu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BookItemVC:UIViewController
+{
+    override func viewDidLoad()
+    {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        
+    }
+}
