b8a33734198333f8fd7a993dd4f48caabc3d7b45
alexinntekt <alex.inntekt@gmail.com>
2019-08-01 08:51:16 +0300

    added my bookings VC

---
+++ play/VCs/MyBookings.swift
@@ -0,0 +1,21 @@
+//
+//  MyBookings.swift
+//  play
+//
+//  Created by Alexandru-Mihai Manolescu on 01/08/2019.
+//  Copyright © 2019 Alexandru-Mihai Manolescu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyBookingsVC: UIViewController
+{
+    override func viewDidLoad() {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+}
