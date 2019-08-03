d617a3ecebfb8160fced02499b459cc9ca02da63
alexinntekt <alex.inntekt@gmail.com>
2019-08-03 15:06:26 +0300

    sp

--- play/VCs/BookItemVC.swift
+++ play/VCs/BookItemVC.swift
@@ -115,7 +115,17 @@ class BookItemVC: UIViewController, UITextFieldDelegate, UITextViewDelegate
                             {
                                 addBooking(itemName: self.currentItem.name, item: self.currentItem.id, of_user_id: current_user_id!, description: self.textfieldDescription.text, in_category: self.currentItem.category, startdate: self.startDateOfBooking, enddate: self.endDateOfBooking)
                 
-                                alert(UIVC: self, title: "Rezervare efectuată", message: "Rezervarea a fost facută cu succes!")
+                                
+                                let title = "Rezervare efectuată"
+                                let message = "Rezervarea a fost facută cu succes!"
+                                let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+                                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { _ in
+                                    self.dismiss(animated: true, completion: nil)
+                                    print("Dismissing VC after adding booking")
+                                }))
+                                self.present(alert, animated: true)
+                                
+                                
                             }
                             else
                             {
