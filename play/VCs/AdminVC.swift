2109ae453d9abf81357501aa38f61b0c88841d57
alexinntekt <alex.inntekt@gmail.com>
2019-08-04 15:32:52 +0300

    spp

---
+++ play/VCs/AdminVC.swift
@@ -0,0 +1,19 @@
+//
+//  AdminVC.swift
+//  play
+//
+//  Created by Alexandru-Mihai Manolescu on 04/08/2019.
+//  Copyright © 2019 Alexandru-Mihai Manolescu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class AdminVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        
+    }
+}
