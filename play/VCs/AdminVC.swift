0ede8244ec512ec785416a11d48f0be6e524a063
alexinntekt <alex.inntekt@gmail.com>
2019-08-05 09:54:01 +0300

    sp

--- play/VCs/AdminVC.swift
+++ play/VCs/AdminVC.swift
@@ -42,72 +42,63 @@ class AdminVC: UIViewController, UITableViewDelegate, UITableViewDataSource
     {
         let currentUserId = Auth.auth().currentUser?.uid
         
-        var bookings = [Booking]()
         
-        var ref = Database.database().reference().child("Users").child(currentUserId!).child("bookings")
-        
-        
-        print("running loadBookings()")
-        
-        
-        //            self.displayingBookings.removeAll()
-        //            bookings.removeAll()
-        
-        ref.observe(.childAdded) { (snap) in
-            //print(snap.key)
+        let ref1 = Database.database().reference().child("Users")
+        ref1.observeSingleEvent(of: .value) { (snap) in
             
-            
-            //self.displayingBookings.removeAll()
-            
-            var bookings = [Booking]()
-            
-            let ref2 = Database.database().reference(withPath: "Bookings").queryOrderedByKey().queryEqual(toValue: snap.key)
-            
-            
-            //                ref2.observeSingleEvent(of: .childAdded , with:
-            //                                    {(snap) in print()
-            //                       print(snap)
-            //                })
-            
-            ref2.observeSingleEvent(of: .childRemoved, with:
-                {(snap) in print()
-                    var int=0;
-                    for obj in self.displayingBookings
-                    {
-                        if(obj.id==snap.key)
-                        {
-                            self.displayingBookings.remove(at: int)
+            for child in snap.children.allObjects as! [DataSnapshot]{
+                
+                let userUid = child.key
+                
+                let ref = Database.database().reference().child("Users").child(userUid).child("bookings")
+                
+                ref.observe(.childAdded) { (snap) in
+                    
+                    let ref2 = Database.database().reference(withPath: "Bookings").queryOrderedByKey().queryEqual(toValue: snap.key)
+                    
+                    
+                    ref2.observeSingleEvent(of: .childRemoved, with:
+                        {(snap) in print()
+                            var int=0;
+                            for obj in self.displayingBookings
+                            {
+                                if(obj.id==snap.key)
+                                {
+                                    self.displayingBookings.remove(at: int)
+                                    self.TBVAdmin.reloadData()
+                                }
+                                int+=1
+                            }
+                    })
+                    
+                    ref2.observeSingleEvent(of: .childAdded , with:
+                        {(snap) in print()
+                            
+                            print("fbuiwgeiwbGIwge")
+                            
+                            let currentBooking = Booking()
+                            currentBooking.category = snap.childSnapshot(forPath: "categoryId").value as! String
+                            currentBooking.description = snap.childSnapshot(forPath: "descriere").value as! String
+                            currentBooking.user = snap.childSnapshot(forPath: "user").value as! String
+                            currentBooking.itemId = snap.childSnapshot(forPath: "itemId").value as! String
+                            currentBooking.itemName = snap.childSnapshot(forPath: "itemName").value as! String
+                            currentBooking.startDate = snap.childSnapshot(forPath: "interval").childSnapshot(forPath: "from").value as! String
+                            currentBooking.endDate = snap.childSnapshot(forPath: "interval").childSnapshot(forPath: "till").value as! String
+                            currentBooking.id = snap.key
+                            
+                            self.displayingBookings.append(currentBooking)
+                            print(currentBooking.description)
                             self.TBVAdmin.reloadData()
-                        }
-                        int+=1
-                    }
-            })
+                            
+                    })
+                }
+            }
             
-            ref2.observeSingleEvent(of: .childAdded , with:
-                {(snap) in print()
-                    
-                    print("fbuiwgeiwbGIwge")
-                    
-                    let currentBooking = Booking()
-                    currentBooking.category = snap.childSnapshot(forPath: "categoryId").value as! String
-                    currentBooking.description = snap.childSnapshot(forPath: "descriere").value as! String
-                    currentBooking.user = snap.childSnapshot(forPath: "user").value as! String
-                    currentBooking.itemId = snap.childSnapshot(forPath: "itemId").value as! String
-                    currentBooking.itemName = snap.childSnapshot(forPath: "itemName").value as! String
-                    currentBooking.startDate = snap.childSnapshot(forPath: "interval").childSnapshot(forPath: "from").value as! String
-                    currentBooking.endDate = snap.childSnapshot(forPath: "interval").childSnapshot(forPath: "till").value as! String
-                    currentBooking.id = snap.key
-                    
-                    self.displayingBookings.append(currentBooking)
-                    print(currentBooking.description)
-                    self.TBVAdmin.reloadData()
-                    
-            })
+ 
         }
-        
-        
-        
     }
-    }
+        
+        
+}
     
 
