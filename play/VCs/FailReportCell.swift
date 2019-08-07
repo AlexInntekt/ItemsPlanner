dcf122a394e303b89a2db70d637f44e21f24587e
alexinntekt <alex.inntekt@gmail.com>
2019-08-07 20:30:39 +0300

    sp

---
+++ play/VCs/FailReportCell.swift
@@ -0,0 +1,17 @@
+//
+//  FailReportCell.swift
+//  play
+//
+//  Created by Alexandru-Mihai Manolescu on 07/08/2019.
+//  Copyright © 2019 Alexandru-Mihai Manolescu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FailReportCell: UITableViewCell {
+
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var phoneNumberLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
--- play/VCs/FailedBookingReport.swift
+++ play/VCs/FailedBookingReport.swift
@@ -9,12 +9,36 @@
 import Foundation
 import UIKit
 
-class FailedBookingReport: UIViewController
+class FailedBookingReport: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
     var report = [FailedBookingReport]()
     var desiredItem = [Item]()
     
+    @IBOutlet var tbv: UITableView!
+    
+    
     override func viewDidLoad() {
+        self.tbv.delegate = self
+        self.tbv.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+         let cell = self.tbv.dequeueReusableCell(withIdentifier: "FailReportCell") as! FailReportCell
+             cell.usernameLabel?.text="tu"
+             cell.phoneNumberLabel?.text="587129537"
+             cell.dateLabel?.text="maine"
+        return UITableViewCell()
+    }
+    
+//    let cell = self.itemsTableView.dequeueReusableCell(withIdentifier: "TBVCell") as! TBVCell
+//    let item = displayingItems[indexPath.row]
+//    cell.labelName?.text = item.name
+//    cell.labelDescription?.text = item.description
+//    cell.labelCategory?.text = "categorie: \(item.category)"
+//    return cell;
+//
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
 }
