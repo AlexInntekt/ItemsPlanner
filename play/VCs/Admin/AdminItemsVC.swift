1624a8fbf0c1d251cd73f6f8ee209f8fc9724930
alexinntekt <alex.inntekt@gmail.com>
2019-08-10 13:09:33 +0300

    admin add item

---
+++ play/VCs/Admin/AdminItemsVC.swift
@@ -0,0 +1,18 @@
+//
+//  AdminItemsVC.swift
+//  play
+//
+//  Created by Alexandru-Mihai Manolescu on 10/08/2019.
+//  Copyright © 2019 Alexandru-Mihai Manolescu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AdminItemsVC: UIViewController
+{
+    override func viewDidLoad()
+    {
+        
+    }
+}
