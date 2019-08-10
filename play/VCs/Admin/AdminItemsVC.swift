e11505591e5041eb130e9322d1509812f5f64045
alexinntekt <alex.inntekt@gmail.com>
2019-08-10 16:45:08 +0300

    sp

--- play/VCs/Admin/AdminItemsVC.swift
+++ play/VCs/Admin/AdminItemsVC.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
 
 class AdminItemsVC: UIViewController,UITableViewDelegate, UITableViewDataSource
 {
@@ -21,10 +24,30 @@ class AdminItemsVC: UIViewController,UITableViewDelegate, UITableViewDataSource
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tbv.dequeueReusableCell(withIdentifier: "AdminItemCell") as! AdminItemCell
-            cell.itemNameLabel?.text=displayingItems[indexPath.row].name
-            cell.categoryLabel?.text=displayingItems[indexPath.row].category
-            cell.descriptionTextView?.text=displayingItems[indexPath.row].description
+            cell.itemNameLabel?.text="Articol: \(displayingItems[indexPath.row].name)"
+            cell.categoryLabel?.text="Categorie: \(displayingItems[indexPath.row].category)"
+            cell.descriptionTextView?.text="Descriere: \(displayingItems[indexPath.row].description)"
         return cell;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        let delete = UITableViewRowAction(style: .destructive, title: "Delete") { (action, indexPath) in
+            let itemToDelete=self.displayingItems[indexPath.row]
+            
+            let ref=Database.database().reference()
+            
+            let path_of_item=ref.child("Categories").child(itemToDelete.category).child("items").child(itemToDelete.id)
+                path_of_item.removeValue()
+            print("tapped delete")
+//            let bk = self.displayingItems[indexPath.row]
+            self.displayingItems.remove(at: indexPath.row)
+            self.tbv.reloadData()
+//            deleteMyBookingWithId(bk_id: bk.id, item_id: bk.itemId, cat: bk.category)
+        }
+        
+        return [delete]
     }
     
     
@@ -34,5 +57,45 @@ class AdminItemsVC: UIViewController,UITableViewDelegate, UITableViewDataSource
     {
         self.tbv.delegate=self
         self.tbv.dataSource=self
+        
+        loadDataFromDB()
+    }
+    
+    func loadDataFromDB()
+    {
+        items.removeAll()
+        displayingItems.removeAll()
+        //displayingCategories.removeAll()
+        
+        let ref = Database.database().reference().child("Categories")
+        
+        ref.observe(.value) { (allCategories) in
+            
+            for category in allCategories.children.allObjects as! [DataSnapshot]{
+                displayingCategories.append(category.key)
+                
+                let packets = category.childSnapshot(forPath: "items")
+                //print(packets)
+                
+                for item in packets.children.allObjects as! [DataSnapshot]
+                {
+                    let currentItem = Item()
+                    currentItem.category = category.key as! String
+                    currentItem.description = item.childSnapshot(forPath: "descriere").value as! String
+                    currentItem.name = item.childSnapshot(forPath: "name").value as! String
+                    currentItem.id = item.key
+                    
+                    print(currentItem.category)
+                    
+                    self.items.append(currentItem)
+                    self.displayingItems.append(currentItem)
+                    
+                }
+            }
+            
+            self.tbv.reloadData()
+//            self.pickerCategory.reloadAllComponents()
+        }
+        
     }
 }
